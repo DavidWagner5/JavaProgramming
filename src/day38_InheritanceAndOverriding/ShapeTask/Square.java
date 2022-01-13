798980ef08f977bcb3d2abf39ef37df6566bce28
David Wagner <davewgnr@yahoo.com>
2022-01-13 13:56:03 -0600

    commited new change

---
+++ src/day38_InheritanceAndOverriding/ShapeTask/Square.java
@@ -0,0 +1,28 @@
+package day38_InheritanceAndOverriding.ShapeTask;
+
+public class Square extends Shape {
+
+    public double side;
+    public Square(double side) {
+        super("Square");
+        this.side = side;
+    }
+    @Override public double area() {
+        return side*side;
+    }
+    @Override
+    public double perimeter() {
+        return side*4;
+    }
+    @Override
+    public String toString() {
+        return "Square{" +
+                "name='" + name + '\'' +
+                ", side=" + side +
+                " Area = " + area() +
+                " Perimeter= " + perimeter() +
+                '}';
+    }
+
+
+}
